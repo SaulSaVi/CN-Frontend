f11307d99c9305cc0d33700565181100b6776806
SaulSaVi <1575saavedra@gmail.com>
2020-06-12 20:54:31 -0500

    configuracion e instal de reactstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navigation from './components/Navigation'
 
 function App() {
   return (
     <React.Fragment>
+      <Navigation />
       <h1>Hola mundo</h1>
     </React.Fragment>
   );
